54c672b10aa18a09da9e7e5e1f319b506b40036d
Luiz Felipe A. Oliveira <67592458+Felip-Oliv@users.noreply.github.com>
2023-01-29 20:00:08 -0300

    Has

--- 1/map_set_begin.js
+++ 1/map_set_begin.js
@@ -8,10 +8,9 @@
 
 const saturday = new Map();
 
-saturday.set(8, 'wall the dog');
+saturday.set(9, 'wall the dog');
 saturday.set(12, 'lunch');
 saturday.set(3, "watch a movie");
-
 
 console.log(saturday)
 
@@ -23,5 +22,18 @@ console.log("What am i doing at 12pm  on Saturday", noon);
 //How do we get the value associated with the 9 key?
 const nine = saturday.get(9);
 console.log("What am i doing at 12pm  on Saturday", nine);
+
+
+
+//Does your map have a value associated with 4 key?
+const hasFour = saturday.has(4);
+console.log("Does our map have four?", hasFour);
+
+
+//Does your map have a value associated with 8 key?
+
+const hasEight = saturday.has(4);
+console.log("Does our map have four?", hasEight);
+
 
 
